7667097c06024872d0eefe7faf08a81e20f15e38
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2024-10-28 19:54:16 +0500

    Task4

---
+++ Tyuiu.BuyanovaVI.Sprint2.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+using Tyuiu.BuyanovaVI.Sprint2.Task4.V10.Lib;
+
+namespace Tyuiu.BuyanovaVI.Sprint2.Task4.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+
+       
+            [TestMethod]
+            public void ValidCondition1()
+            {
+                DataService ds = new DataService();
+                double x = 1;
+                double y = 9;
+                double res = ds.Calculate(x, y);
+                double wait = 1.037;
+                Assert.AreEqual(wait, res);
+            }
+            [TestMethod]
+            public void ValidCondition2()
+            {
+                DataService ds = new DataService();
+                double x = 5;
+                double y = 6;
+                double res = ds.Calculate(x, y);
+                double wait = 55;
+                Assert.AreEqual(wait, res);
+            }
+  
+    }
+}
---
+++ Tyuiu.BuyanovaVI.Sprint2.Task4.V10/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using Tyuiu.BuyanovaVI.Sprint2.Task4.V10.Lib;
+
+namespace Tyuiu.BuyanovaVI.Sprint2.Task4.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнилa: Буянова В. И. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнилa: Буянова В. И. | СМАРТб-24-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарного оператора, где пользователь вводит            *");
+            Console.WriteLine("* значение переменной X,Y с клавиатуры, если x*2 < y/3, то  z=(1+3/y^2 )^x*");
+            Console.WriteLine("* иначе y +10*x - (5/x)                                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
+        }
+    }
+}
